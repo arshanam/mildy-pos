37ebadc7199854113b04a296edeeab6afd6b9c6c
DanX <danny@danny-Presario-V3700-Notebook-PC.(none)>
2013-11-24 15:57:33 +0700

    modify views users

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,7 +1,13 @@
 @extends('master')
 
 @section('content')
-	<div>
-		
-	</div>
+	 <div class="row">
+ 	<div class="span offset4">
+ 		<div class="well">
+	{{ Form::open(array('url' => '')) }}
+    {{ Form::text('username', '', array('class'=>'form-control', 'placeholder'=>'Username')); }}<br>
+    {{ Form::password('password', array('class'=>'form-control', 'placeholder'=>'Password')); }}<br>
+    {{ Form::submit('Login', array('class'=>'btn btn-primary')) }}
+	{{ Form::close() }}
+	</div></div></div>
 @stop
