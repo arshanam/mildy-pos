37dd38f9be224741deeb819200572edceb7baad3
ibrahimyu <ibrahim7usuf@gmail.com>
2013-11-24 15:16:56 +0700

    Update UserController + lang

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -2,9 +2,26 @@
 
 class UserController extends BaseController {
 
+	public function showLogin()
+	{
+		return View::make('users.login');
+	}
+
 	public function login()
 	{
-		//return View::make('hello');
+		if (Auth::attempt(array('email' => $email, 'password' => $password))) {
+			return Redirect::to('/');
+		}
+		else
+			return Redirect::back()->withInput()->with('message', trans('login.login_error'));
+	}
+
+	public function logout()
+	{
+		if (Auth::check())
+			Auth::logout();
+
+		return Redirect::to('/');
 	}
 
 }
---
+++ app/lang/en/login.php
@@ -0,0 +1,5 @@
+<?php 
+
+return array(
+	'login_error': 'Username or password do not match. Please try again.',
+);
