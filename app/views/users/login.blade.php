a37597c011e97f2a2978bf3d33477def4f7e37d9
ibrahimyu <ibrahim7usuf@gmail.com>
2013-11-29 10:52:39 +0700

    Fix login page parent layout

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,22 +1,24 @@
 @extends('master')
 
-@section('content')
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4">
-			<div class="well">
-				<h3>Login</h3>				
-				
-				@if (Session::has('message'))
-					<div class="alert alert-danger">
-						{{ Session::get('message') }}
-					</div>
-				@endif
-				
-				{{ Form::open(array('url' => '/login', 'method' => 'post')) }}
-					{{ Form::text('username', '', array('class'=>'form-control', 'placeholder'=>'Username')); }}<br>
-					{{ Form::password('password', array('class'=>'form-control', 'placeholder'=>'Password')); }}<br>
-					{{ Form::submit('Login', array('class'=>'btn btn-primary')) }}
-				{{ Form::close() }}
+@section('body')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="well">
+					<h3>Login</h3>				
+					
+					@if (Session::has('message'))
+						<div class="alert alert-danger">
+							{{ Session::get('message') }}
+						</div>
+					@endif
+					
+					{{ Form::open(array('url' => '/login', 'method' => 'post')) }}
+						{{ Form::text('username', '', array('class'=>'form-control', 'placeholder'=>'Username')); }}<br>
+						{{ Form::password('password', array('class'=>'form-control', 'placeholder'=>'Password')); }}<br>
+						{{ Form::submit('Login', array('class'=>'btn btn-primary')) }}
+					{{ Form::close() }}
+				</div>
 			</div>
 		</div>
 	</div>
