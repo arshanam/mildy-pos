94f4961046e72eb0e42e7f6fd24fa29dc775a0a6
DannyChayadi <danny.chayadi@gmail.com>
2013-12-14 10:44:19 +0700

    modify staffs.form

--- app/views/staffs/form.blade.php
+++ app/views/staffs/form.blade.php
@@ -13,29 +13,37 @@
 	@endif
 
 	{{ Form::open(array('url' => '/staffs', 'method' => 'post')) }}
-		{{ Form::label('username', 'Username') }}
-		{{ Form::text('username', Input::old('username'), array('class'=>'form-control')); }}<br>
-		@if ($errors->has('username'))
-			<span class="help-block">{{ $errors->first('username') }}</span>
-		@endif
+		<div class="form-group {{ $errors->has('username') ? 'has-error' : '' }}">
+			{{ Form::label('username', 'Username') }}
+			{{ Form::text('username', Input::old('username'), array('class'=>'form-control')); }}
+			@if ($errors->has('username'))
+				<span class="help-block">{{ $errors->first('username') }}</span>
+			@endif
+		</div>
 
-		{{ Form::label('password', 'Password') }}
-		{{ Form::password('password', array('class'=>'form-control')); }}<br>
-		@if ($errors->has('password'))
-			<span class="help-block">{{ $errors->first('password') }}</span>
-		@endif
+		<div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
+			{{ Form::label('password', 'Password') }}
+			{{ Form::password('password', array('class'=>'form-control')); }}
+			@if ($errors->has('password'))
+				<span class="help-block">{{ $errors->first('password') }}</span>
+			@endif
+		</div>
 
-		{{ Form::label('name', 'Name') }}
-		{{ Form::text('name', Input::old('name'), array('class'=>'form-control')); }}<br>
-		@if ($errors->has('name'))
-			<span class="help-block">{{ $errors->first('name') }}</span>
-		@endif
+		<div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+			{{ Form::label('name', 'Name') }}
+			{{ Form::text('name', Input::old('name'), array('class'=>'form-control')); }}
+			@if ($errors->has('name'))
+				<span class="help-block">{{ $errors->first('name') }}</span>
+			@endif
+		</div>
 
-		{{ Form::label('email', 'Email') }}
-		{{ Form::email('email', Input::old('email'), array('class'=>'form-control')); }}<br>
-		@if ($errors->has('email'))
-			<span class="help-block">{{ $errors->first('email') }}</span>
-		@endif
+		<div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+			{{ Form::label('email', 'Email') }}
+			{{ Form::email('email', Input::old('email'), array('class'=>'form-control')); }}
+			@if ($errors->has('email'))
+				<span class="help-block">{{ $errors->first('email') }}</span>
+			@endif
+		</div>
 
 		{{ Form::submit('Save', array('class'=>'btn btn-primary')) }}
 		<a href="/staffs" class="btn btn-default">Cancel</a>
